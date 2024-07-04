f3d523bdb226bfc909ca9bc7e6b79ad37e711c61
zyounee <jiyoon778@gmail.com>
2024-07-04 22:20:53 +0900

    팀원 검색

--- src/main/java/com/ppyongppyong/server/user/dto/UserDataDto.java
+++ src/main/java/com/ppyongppyong/server/user/dto/UserDataDto.java
@@ -1,2 +1,19 @@
-package com.ppyongppyong.server.user.dto;public class UserDataDto {
+package com.ppyongppyong.server.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class UserDataDto {
+
+    private Long id;
+    private String userId;
+    private String userName;
+
+    @Builder
+    public UserDataDto(Long id, String userId, String userName){
+        this.id = id;
+        this.userId = userId;
+        this.userName = userName;
+    }
 }
