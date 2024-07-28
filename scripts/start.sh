3fc42b28e5ae19f0077554e46d61fc6c07e45f91
choincnp <yjcho@steg.co.kr>
2024-07-29 00:53:16 +0900

    [BY-27]fix: change scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,8 +11,15 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 # build 파일 복사
-echo "$TIME_NOW > $TARGET_JAR 파일 복사" >> $DEPLOY_LOG
-cp $BUILD_LIBS/*.jar $TARGET_JAR
+echo "$TIME_NOW > $BUILD_LIBS 에서 $TARGET_JAR 파일로 복사" >> $DEPLOY_LOG
+cp $BUILD_LIBS/server-0.0.1-SNAPSHOT.jar $TARGET_JAR
+
+# 기존에 실행 중인 애플리케이션 종료
+CURRENT_PID=$(pgrep -f $TARGET_JAR)
+if [ -n "$CURRENT_PID" ]; then
+  echo "$TIME_NOW > 실행 중인 애플리케이션 종료: $CURRENT_PID" >> $DEPLOY_LOG
+  kill -15 $CURRENT_PID
+fi
 
 # jar 파일 실행
 echo "$TIME_NOW > $TARGET_JAR 파일 실행" >> $DEPLOY_LOG
