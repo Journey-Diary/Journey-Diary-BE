b2c1a9b58918e510c097c288d5dd2ba355f9c49f
choincnp <yjcho@steg.co.kr>
2024-07-29 00:48:29 +0900

    [BY-27]fix: change scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/build/libs/server-0.0.1-SNAPSHOT.jar"
+BUILD_LIBS="$PROJECT_ROOT/build/libs"
+TARGET_JAR="$PROJECT_ROOT/spring-webapp.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -10,12 +11,12 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 # build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+echo "$TIME_NOW > $TARGET_JAR 파일 복사" >> $DEPLOY_LOG
+cp $BUILD_LIBS/*.jar $TARGET_JAR
 
 # jar 파일 실행
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar -Duser.timezone=Asia/Seoul $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+echo "$TIME_NOW > $TARGET_JAR 파일 실행" >> $DEPLOY_LOG
+nohup java -jar -Duser.timezone=Asia/Seoul $TARGET_JAR > $APP_LOG 2> $ERROR_LOG &
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f $TARGET_JAR)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
